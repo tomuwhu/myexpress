a650bfda770fc529d58edc634f1f2f8b2a5cafc2
Dr. Németh Tamás <tomuwhu@gmail.com>
2024-10-19 13:34:51 +0200

    .

--- index.js
+++ index.js
@@ -2,17 +2,15 @@ const express = require('express')
 const app = express()
 const port = 3000
 const cors = require('cors')
-
+app.use(express.json())
 app.use(cors())
-
 app.get('/', (req, res) => {
   res.send({x: 1})
 })
-
 app.post('/', (req, res) => {
+    console.log(req.body)
     res.send({x: 'Post'})
 })
-
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
